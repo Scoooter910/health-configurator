ee598779b2ac6bcd64cbb501f81411f4f157f611
Michele Mostoller <MMostoller@att.net>
2025-06-17 17:19:11 -0400

    Update settings.py
    Removed generic key

--- client_portal/settings.py
+++ client_portal/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cul8k82p%v*ag*foeeq^yj8r@do-cbqkx0ylame)$m3-41g)(4'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
